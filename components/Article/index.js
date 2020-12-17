2783c921d5f0f5a4928bfc9bf0e59fd4976dd196
nolotus <s@nolotus.com>
2020-12-17 22:13:07 +0800

    better css style

--- components/Article/index.js
+++ components/Article/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { CurrentEditor } from "../../components/Editor";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+const StyledArticle = styled.div`
+  width: 90%;
+  max-width: 1050px;
+  margin: 3em auto 0;
+`;
 export const Article = (props) => {
-  const {doc} =props
-  console.log('doc',doc)
+  const { doc } = props;
+  console.log("doc", doc);
   return (
-      <CurrentEditor
-        value={doc.content}
-      />
-   
+    <StyledArticle>
+      <CurrentEditor value={doc.content} />
+    </StyledArticle>
   );
 };
 Article.propTypes = {
--- pages/Life.js
+++ pages/Life.js
@@ -1,17 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { localDb } from "../common/db";
+import { useDispatch } from "react-redux";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { hostDb } from "../common/db";
 import Template from "../template";
 import { dbAll } from "../common/api";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import ArticleTitle from "../components/ArticleTitle";
-import { useDispatch } from "react-redux";
 
+const StyledLife = styled.div`
+  width: 90%;
+  max-width: 1050px;
+  margin: 3em auto 0;
+`;
 const Life = () => {
   const [list, setList] = useState([]);
+  const [currentDb, setCurrentDb] = useState(hostDb.remote);
   const dispatch = useDispatch();
   const getData = () => {
-    dbAll(localDb).then((result) => {
+    dbAll(currentDb).then((result) => {
       if (result) {
         console.log("result", result);
         setList(result.rows);
@@ -19,21 +27,25 @@ const Life = () => {
     });
   };
   const goDelete = (id) => {
-    localDb.get(id).then(async (doc) => {
-      const result = await localDb.remove(doc);
-      console.log("handleDetele", result);
-      getData();
-      closeModal()
-    }).catch(function (err) {
-      console.log(err);
-    });
+    currentDb
+      .get(id)
+      .then(async (doc) => {
+        const result = await currentDb.remove(doc);
+        console.log("handleDetele", result);
+        getData();
+        closeModal();
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
   };
   const modal = (modalInfo) => {
     dispatch({ type: "modal", modalInfo: { ...modalInfo, isVisible: true } });
   };
-  const closeModal =()=> dispatch({ type: "modal", modalInfo: { isVisible: false } })
+  const closeModal = () =>
+    dispatch({ type: "modal", modalInfo: { isVisible: false } });
   useEffect(() => {
-    dbAll(localDb).then((result) => {
+    dbAll(currentDb).then((result) => {
       if (result) {
         console.log("result", result);
         setList(result.rows);
@@ -65,24 +77,26 @@ const Life = () => {
   };
   return (
     <Template>
-      {list.map((post) => (
-        <ArticleTitle key={post.id}>
-          <div>
-            <Link to={{ pathname: post.doc._id }}>
-              {post.doc.title || post.doc._id}
-            </Link>
-          </div>
+      <StyledLife>
+        {list.map((post) => (
+          <ArticleTitle key={post.id}>
+            <div>
+              <Link to={{ pathname: post.doc._id }}>
+                {post.doc.title || post.doc._id}
+              </Link>
+            </div>
 
-          <div
-            className="delete-button"
-            onClick={() => handleDetele(post.doc._id)}
-          >
-            <FontAwesomeIcon icon={["fas", "times"]} />
-          </div>
+            <div
+              className="delete-button"
+              onClick={() => handleDetele(post.doc._id)}
+            >
+              <FontAwesomeIcon icon={["fas", "times"]} />
+            </div>
 
-          {/*<p>{post.doc.content}</p>*/}
-        </ArticleTitle>
-      ))}
+            {/*<p>{post.doc.content}</p>*/}
+          </ArticleTitle>
+        ))}
+      </StyledLife>
     </Template>
   );
 };
