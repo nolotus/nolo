04f5800aded5844abfda8789109e85b0618e5437
nolotus <s@nolotus.com>
2020-12-24 10:22:14 +0800

    add config

---
+++ server/config.js
@@ -0,0 +1,4 @@
+const env = process.env.NODE_ENV;
+
+export const isProdEnv = (env === 'production');
+export const isDevEnv = (env === 'development');
--- server/index.js
+++ server/index.js
@@ -3,9 +3,10 @@ import render from "./render";
 import { matchRoutes } from "react-router-config";
 import Routes from "../common/Routes";
 import store from "../common/store";
+import {isProdEnv} from './config'
 const app = express();
-var proxy = require('express-http-proxy');
-app.use('/db', proxy('http://localhost:5984'));
+var proxy = require("express-http-proxy");
+app.use("/db", proxy("http://localhost:5984"));
 app.use(express.static("public"));
 app.get("*", (req, res) => {
   const promises = matchRoutes(Routes, req.path).map(({ route }) => {
@@ -17,17 +18,23 @@ app.get("*", (req, res) => {
     res.send(html);
   });
 });
-require("greenlock-express")
-    .init({
-        packageRoot: process.cwd(),
-        configDir: "./greenlock.d",
- 
-        // contact for security and critical bug notices
-        maintainerEmail: "s@nolotus.com",
- 
-        // whether or not to run at cloudscale
-        cluster: false
-    })
-    // Serves on 80 and 443
-    // Get's SSL certificates magically!
-    .serve(app);
+
+if(isProdEnv){
+  require("greenlock-express")
+  .init({
+    packageRoot: process.cwd(),
+    configDir: "./greenlock.d",
+
+    // contact for security and critical bug notices
+    maintainerEmail: "s@nolotus.com",
+
+    // whether or not to run at cloudscale
+    cluster: false,
+  })
+  // Serves on 80 and 443
+  // Get's SSL certificates magically!
+  .serve(app);
+
+}else{
+  app.listen(80, () => console.log("Example app listening on port 80!"));
+}
