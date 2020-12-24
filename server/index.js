66dad03b622aeb6994c2966d086505cac8335df9
nolotus <s@nolotus.com>
2020-12-24 12:02:31 +0800

    new proxy type

--- server/index.js
+++ server/index.js
@@ -1,44 +1,57 @@
-import express from "express";
-import render from "./render";
 import { matchRoutes } from "react-router-config";
 import Routes from "../common/Routes";
 import store from "../common/store";
-import { isDevEnv } from "./config";
-const app = express();
-var proxy = require("express-http-proxy");
-// app.use("/db", );
-app.use(express.static("public"));
-app.get("*", (req, res) => {
-  console.log("req", req.hostname);
-  if (req.hostname === "tw.db.nolotus.com") {
-    proxy("http://localhost:5984");
-  } else {
-    const promises = matchRoutes(Routes, req.path).map(({ route }) => {
-      const component = route.component;
-      return component.getInitialData ? component.getInitialData(store) : null;
-    });
-    Promise.all(promises).then(() => {
-      const html = render(req, store);
-      res.send(html);
-    });
-  }
-});
+import render from "./render";
 
-if (isDevEnv) {
-  app.listen(80, () => console.log("Example app listening on port 80!"));
-} else {
-  require("greenlock-express")
-    .init({
-      packageRoot: process.cwd(),
-      configDir: "./greenlock.d",
-
-      // contact for security and critical bug notices
-      maintainerEmail: "s@nolotus.com",
-
-      // whether or not to run at cloudscale
-      cluster: false,
+require("greenlock-express")
+    .init(function getConfig() {
+        // Greenlock Config
+        return {
+            packageRoot: process.cwd(),
+            configDir: "./greenlock.d",
+            maintainerEmail: "s@nolotus.com",
+            cluster: false
+        };
     })
-    // Serves on 80 and 443
-    // Get's SSL certificates magically!
-    .serve(app);
+    .ready(httpsWorker);
+
+function httpsWorker(glx) {
+    // we need the raw https server
+    var server = glx.httpsServer();
+    var proxy = require("http-proxy").createProxyServer({ xfwd: true });
+
+    // catches error events during proxying
+    proxy.on("error", function(err, req, res) {
+        console.error(err);
+        res.statusCode = 500;
+        res.end();
+        return;
+    });
+
+    // We'll proxy websockets too
+    server.on("upgrade", function(req, socket, head) {
+        proxy.ws(req, socket, head, {
+            ws: true,
+            target: "ws://localhost:3000"
+        });
+    });
+    // servers a node app that proxies requests to a localhost
+    glx.serveApp(function(req, res) {
+        if (req.hostname === "tw.db.nolotus.com") { 
+        proxy.web(req, res, {
+            target: "http://localhost:5984"
+        });}
+        else{
+            const promises = matchRoutes(Routes, req.path).map(({ route }) => {
+                const component = route.component;
+                return component.getInitialData ? component.getInitialData(store) : null;
+              });
+              Promise.all(promises).then(() => {
+                const html = render(req, store);
+                res.send(html);
+              });
+
+        }
+  
+    });
 }
