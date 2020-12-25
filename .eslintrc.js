6686912533c1c0cbd42f68ec1f5a4dd5d0cb2cd4
nolotus <s@nolotus.com>
2020-12-25 13:46:37 +0800

    add react-native

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,21 @@
 module.exports = {
-  "parserOptions": {
-    "ecmaVersion": 2018,
-    "sourceType": "module"
-},
-"env": {
-  "browser": true,
-  "node": true,
-  "es6": true
-},
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings"
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    '@react-native-community',
   ],
   rules: {
     'no-console': 'off',
