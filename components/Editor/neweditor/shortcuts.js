c07e205aa68df9243913f2b4f92b378b0c058a3e
nolotus <s@nolotus.com>
2021-01-24 18:51:19 +0800

    format code

---
+++ components/Editor/neweditor/shortcuts.js
@@ -0,0 +1,12 @@
+const SHORTCUTS = {
+  '*': 'list-item',
+  '-': 'list-item',
+  '+': 'list-item',
+  '>': 'block-quote',
+  '#': 'heading-one',
+  '##': 'heading-two',
+  '###': 'heading-three',
+  '####': 'heading-four',
+  '#####': 'heading-five',
+  '######': 'heading-six',
+};
