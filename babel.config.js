978a7baea8449561600435293a984aee93f7e39c
nolotus <s@nolotus.com>
2021-01-30 15:38:10 +0800

    add babel

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-  "presets": [
-    "@babel/preset-env",
-    "@babel/preset-react",
-    'module:metro-react-native-babel-preset'
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    'module:metro-react-native-babel-preset',
   ],
-  "plugins": [
-    [
-      "@babel/plugin-transform-runtime"
-    ]
-  ]
+  plugins: [
+    ['babel-plugin-styled-components'],
+    ['@babel/plugin-transform-runtime'],
+  ],
 };
